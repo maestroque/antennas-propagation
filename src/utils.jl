680e127dc2900ea6bc697b64c7552219c5cfb205
maestroque <kikasgeorg@ece.auth.gr>
2023-12-17 21:39:01 +0200

    Add polar plot utils

--- src/utils.jl
+++ src/utils.jl
@@ -15,13 +15,24 @@ function radiationPatternPlot3D(antenna::AntennaArray, precision::Float64)
     ys = [Y(fieldIntensity(antenna, theta, phi), theta, phi) for theta in θ, phi in ϕ]
     zs = [Z(fieldIntensity(antenna, theta, phi), theta, phi) for theta in θ, phi in ϕ]
 
-    PlotlyJS.plot(PlotlyJS.surface(x=xs, y=ys, z=zs, surfacecolor=@. xs^2 + ys^2 + zs^2))
+    p = PlotlyJS.plot(PlotlyJS.surface(x=xs, y=ys, z=zs, surfacecolor=@. xs^2 + ys^2 + zs^2))
+    display(p)
 end
 
-function radiationPatternPlotVertical(antenna::AntennaArray)
-
+function radiationPatternPlotVertical(antenna::AntennaArray, precision::Float64)
+    θ = 0:precision:2π
+    ϕ = 0:precision:π
+    
+    p = Plots.plot(ϕ, fieldIntensity.(Ref(antenna), π/2, ϕ), proj=:polar, gridlinewidth=2)
+    # p = PlotlyJS.plot(PlotlyJS.scatterpolar(ϕ, fieldIntensity.(Ref(antenna), π/2, ϕ), theta=:y, mode="lines"))
+    display(p)
 end
 
-function radiationPatternPlotHorizontal(antenna::AntennaArray)
+function radiationPatternPlotHorizontal(antenna::AntennaArray, precision::Float64)
+    θ = 0:precision:2π
+    ϕ = 0:precision:π
+    p = Plots.plot(θ, fieldIntensity.(Ref(antenna), θ, 0), proj=:polar, gridlinewidth=2)
+    # p = PlotlyJS.plot(PlotlyJS.scatterpolar(ϕ, fieldIntensity.(Ref(antenna), θ, 0), theta=:y, mode="lines"))
 
+    display(p)
 end
--- test/uniformArray.jl
+++ test/uniformArray.jl
@@ -6,7 +6,9 @@ includet("../src/utils.jl")
 
 antennaBase = createAntennaArray(1e9, 1, 0.5, 0.5, 0.5, 16, 12, 0, 0)
 
-steered60 = steerAntennaArray(antennaBase, π/3)
+steered = steerAntennaArray(antennaBase, π/2, π/3)
 
-radiationPatternPlot3D(steered60, 0.01)
+radiationPatternPlot3D(steered, 0.01)
+radiationPatternPlotHorizontal(steered, 0.01)
+# radiationPatternPlotVertical(steered, 0.01)
 
