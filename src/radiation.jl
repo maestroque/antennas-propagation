22b784b38fd3af59df3be772ca925cca6a7675c7
maestroque <kikasgeorg@ece.auth.gr>
2023-12-19 14:54:51 +0200

    Add directivity calculation via riemann sums

--- src/radiation.jl
+++ src/radiation.jl
@@ -4,6 +4,17 @@ using PlotlyJS
 
 includet("antennaArray.jl")
 
+# HPBW values taken from the HPBW-Antenna Length-θ_0 chart
+HPBW = Dict{Tuple{Any, Any}, Any}()
+HPBW[(0.5, 0)] = 37
+HPBW[(0.5, π/6)] = 13
+HPBW[(0.5, π/3)] = 7
+HPBW[(0.5, π/2)] = 6
+HPBW[(0.25, 0)] = 50
+HPBW[(0.25, π/6)] = 22
+HPBW[(0.25, π/3)] = 14
+HPBW[(0.25, π/2)] = 10
+
 function fieldIntensity(array::AntennaArray2D, θ, ϕ)
     η_0 = 120π
     k = 2π / array.λ
@@ -13,34 +24,41 @@ function fieldIntensity(array::AntennaArray2D, θ, ϕ)
     return abs(E_0) * arrayFactor(array, θ, ϕ)
 end
 
-function directivity(array::AntennaArray2D, θ0::Float64)
+function directivityD(array::AntennaArray2D, θ0::Float64, ϕ0::Float64)
     
-    arrayX = createAntennaArray1D(3e8 / array.λ, array.I, array.l, 
-                                  array.d_x, array.N_x, array.δx)
-    arrayY = createAntennaArray1D(3e8 / array.λ, array.I, array.l, 
-                                  array.d_y, array.N_y, array.δy)
-    D_x = sincDirectivity(arrayX)
-    D_y = sincDirectivity(arrayY)
+    arrayX = createAntennaArray1D(3e8 / array.λ, array.I, array.l / array.λ, 
+                                  array.d_x / array.λ, array.N_x, array.δx)
+    arrayY = createAntennaArray1D(3e8 / array.λ, array.I, array.l / array.λ, 
+                                  array.d_y / array.λ, array.N_y, array.δy)
+    D_x = directivityD(arrayX, ϕ0)
+    D_y = directivityD(arrayY, θ0)
+    println(D_x)
+    println(D_y)
 
-    return π * θ0 * D_x * D_y
+    return π * cos(ϕ0) * D_x * D_y
 end
 
-function chartDirectivity(array::AntennaArray1D, θ0::Float64)
-    # HPBW values taken from the HPBW-Antenna Length-θ_0 chart
-    HPBW = Dict{Tuple{Any, Any}, Any}()
-    HPBW[(0.5, 0)] = 37
-    HPBW[(0.5, π/6)] = 13
-    HPBW[(0.5, π/3)] = 7
-    HPBW[(0.5, π/2)] = 6
-    HPBW[(0.25, 0)] = 50
-    HPBW[(0.25, π/6)] = 22
-    HPBW[(0.25, π/3)] = 14
-    HPBW[(0.25, π/2)] = 10
-    
+function directivityD(array::AntennaArray1D, θ0::Float64)
     D_0 = 2 * array.N * array.d / array.λ
     
     return D_0 * HPBW[(array.d / array.λ, 0)] / HPBW[(array.d / array.λ, θ0)]
+end
 
+function directivityHPBW(array::AntennaArray2D, θ0::Float64, ϕ0::Float64)
+    arrayX = createAntennaArray1D(3e8 / array.λ, array.I, array.l / array.λ, 
+                                  array.d_x / array.λ, array.N_x, array.δx)
+    arrayY = createAntennaArray1D(3e8 / array.λ, array.I, array.l / array.λ, 
+                                  array.d_y / array.λ, array.N_y, array.δy)
+    
+    Θ_x = deg2rad(HPBW[(arrayX.d / arrayX.λ, ϕ0)])
+    Θ_y = deg2rad(HPBW[(arrayY.d / arrayY.λ, θ0)])
+
+    Θ_h = deg2rad(1 / (cos(θ0) * sqrt(cos(ϕ0)^2 / Θ_x^2 + sin(ϕ0)^2 / Θ_y^2)))
+    Ψ_h = deg2rad(1 / sqrt(sin(ϕ0)^2 / Θ_x^2 + cos(ϕ0)^2 / Θ_y^2))
+    println(Θ_h)
+    println(Ψ_h)
+
+    return 32400 / (Θ_h * Ψ_h)
 end
 
 function sincDirectivity(array::AntennaArray1D)
@@ -55,6 +73,27 @@ end
 """
 Directivity calculation using the definition and Riemann Sums
 """
-function riemannDirectivity(array::AntennaArray)
-    
+function riemannDirectivity(array::AntennaArray, precision::Float64)
+    Δϕ = precision
+    Δθ = precision
+    ϕ = 0:precision:2π
+    θ = 0:precision:2π
+
+    η_0 = 120π
+    k = 2π / array.λ
+    r = 1
+
+    E_0 = im * (η_0 * array.I * k * array.l) / (4π * r) * exp(-im * k * r)
+
+    integral = 0
+    for i in ϕ
+        for j in θ
+            integral += fieldIntensity(array, j, i)^2 * sin(j) * Δϕ * Δθ
+        end
+    end
+
+    A = [arrayFactor(array, i, j) for i in ϕ, j in θ]
+
+    return 4π * abs(E_0)^2 * maximum(abs.(A))^2 / integral
 end
+
--- test/uniformArray.jl
+++ test/uniformArray.jl
@@ -5,12 +5,20 @@ includet("../src/antennaArray.jl")
 includet("../src/radiation.jl")
 includet("../src/utils.jl")
 
-antennaBase = createAntennaArray2D(1e9, 1, 0.5, 0.25, 0.25, 16, 12, 0, 0)
-steered = steerAntennaArray(antennaBase, π/2, π/3)
+beam_theta = π/2
+beam_phi = π/3
+d = 0.25
+
+antennaBase = createAntennaArray2D(1e9, 1, 0.5, d, d, 16, 12, 0, 0)
+steered = steerAntennaArray(antennaBase, beam_theta, beam_phi)
 
 # radiationPatternPlot3D(steered, 0.01, "3D Radiation Plot - 0 degrees")
 # radiationPatternPlotHorizontal(steered, 0.01, "Vertical Radiation Plot - 0 degrees")
 # radiationPatternPlotVertical(steered, 0.01, "Horizontal Radiation Plot - 0 degrees")
 
-print("Directivity is: ", directivity(steered, π/3))
+println("Directivity is (using D): ", 10log10(directivityD(steered, beam_theta, beam_phi)))
+println("Directivity is (using HPBW): ", 10log10(directivityHPBW(steered, beam_theta, beam_phi)))
+println("Directivity (Riemann Sums) is: ", 10log10(riemannDirectivity(steered, 0.1)))
+
+
 
